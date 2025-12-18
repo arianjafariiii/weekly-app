9d627432715c65029ba954e264f182d82eb06958
ArianJafari <arian.jafari2233@gmail.com>
2025-12-18 17:27:50 +0330

    Ticket component added to app completely

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
 
+  detailsVidible = signal(false);
+
+  close = output<string>();
+
+  onToggleVisible() {
+    this.detailsVidible.update(wasVisible => !wasVisible);
+  }
+
+  onCloseTicket() {
+    this.close.emit(this.data().id)
+  }
 }
